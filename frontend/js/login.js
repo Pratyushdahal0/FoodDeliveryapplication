83974d1f89ccf10e077454d4e6c399e21733581c
Pratyush <pratyushdahal33@gmail.com>
2026-04-05 09:23:17 +0545

    created login.js

---
+++ frontend/js/login.js
@@ -0,0 +1,18 @@
+function switchTab(tab) {
+  const loginForm = document.getElementById('loginForm');
+  const registerForm = document.getElementById('registerForm');
+  const loginTab = document.getElementById('loginTab');
+  const registerTab = document.getElementById('registerTab');
+
+  if (tab === 'login') {
+    loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+    loginTab.classList.add('active');
+    registerTab.classList.remove('active');
+  } else if (tab === 'register') {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+    loginTab.classList.remove('active');
+    registerTab.classList.add('active');
+  }
+}
